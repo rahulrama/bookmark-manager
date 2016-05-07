31474947f3632d43b203d27f49fbac327efa2730
rahulrama <rahulrama@gmail.com>
2016-05-07 14:14:29 +0100

    Step 10: Creating a modular Sinatra app

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ source "https://rubygems.org"
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 
+gem 'sinatra'
+gem 'rspec-sinatra'
+
 group :test do
   gem 'capybara'
   gem 'rspec'
--- spec/viewing_links_spec.rb
+++ spec/viewing_links_spec.rb
@@ -7,7 +7,7 @@ feature 'Viewing links' do
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content(Makers Academy)
+      expect(page).to have_content('Makers Academy')
     end
   end
 end
