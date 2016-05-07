de7db5ce90ce7b42df5ce6916d58c09fabba2a5a
rahulrama <rahulrama@gmail.com>
2016-05-07 16:16:16 +0100

    Step 17: Multiple Tags

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'sinatra'
 gem 'rspec-sinatra'
+gem 'rake'
 
 group :test do
   gem 'capybara'
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,6 +1,6 @@
 feature 'Adding tags' do
 
-  scenario 'I can adda a single tag to a new link' do
+  scenario 'I can add a single tag to a new link' do
     visit '/links/new'
     fill_in 'url', with: 'http://www.makersacademy.com/'
     fill_in 'title', with: 'Makers Academy'
@@ -13,5 +13,16 @@ feature 'Adding tags' do
     within 'ul#links' do
       expect(page).to have_content('education')
     end
+  end
+
+  scenario 'I can add multiple tags to a link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags', with: 'education, ruby'
+
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
   end
 end
