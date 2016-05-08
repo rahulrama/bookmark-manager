691c7819bf0e38b35338d214c3a2ad17843d1903
rahulrama <rahulrama@gmail.com>
2016-05-08 09:36:27 +0100

    Step 20: Improving the sign up workflow

--- app/app.rb
+++ app/app.rb
@@ -1,12 +1,15 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
+
+  register Sinatra::Flash
 
   helpers do
     def current_user
@@ -43,15 +46,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
     password: params[:password],
     password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/links'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/links'
+    else
+      flash.now[:notice] = 'Password and confirmation password do not match'
+      erb :'users/new'
+    end
   end
 
   # start the server if ruby file executed directly
