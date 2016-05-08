f10a852b40a72e51d1e162a75bbca539b39a2b07
rahulrama <rahulrama@gmail.com>
2016-05-08 11:56:31 +0100

    Step 25: Refactoring

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,8 @@ RSpec.configure do |config|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
+  config.order = 'random'
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
